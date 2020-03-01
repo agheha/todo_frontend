7549e716b9776ea20c719b6aca037d351cc2397b
agheha <youbeen0712@gmail.com>
2020-03-01 16:04:25 +0900

    About component 추가 및 axios

---
+++ src/components/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    {{ msg }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'about',
+  data: () => {
+    return {
+      msg: '내가 개발한 To Do 어플입니다. '
+    }
+  }
+}
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495e;
+}
+</style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,24 +1,42 @@
 <template>
   <div class="hello">
     오늘 해야 할 일
-    {{toDoItems}}
+    <ul v-if="toDoItems && toDoItems.length">
+      <li v-for="toDoItem of toDoItems" v-bind:key="toDoItem">
+        {{ toDoItem.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'hello',
   data: () => {
     return {
-      toDoItems: ['1. 밥 먹기.', '2. 잠 자기.']
+      toDoItems: [] // 빈 리스트로 초기화
     }
+  },
+  created() {
+    // 초기화 함수
+    axios
+      .get('http://127.0.0.1:5000/todo/') // http://localhost:5000/todo/에 get call
+      .then(response => {
+        this.toDoItems = response.data.map(r => r.data) // 반환되는 값을 toDoItems에 저장
+      })
+      .catch(e => {
+        console.log('error : ', e)
+      })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -33,6 +51,6 @@ li {
 }
 
 a {
-  color: #35495E;
+  color: #35495e;
 }
 </style>
